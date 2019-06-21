fd1f6c5cfdc58e8c91faa8a5249be3eb259a4eab
Sujan Prajapati <sujan@thewebsitemarketinggroup.com.au>
2019-06-21 16:37:09 +1000

    passport frontend

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,4 +1,7 @@
 <template>
+    <passport-clients></passport-clients>
+    <passport-authorized-clients></passport-authorized-clients>
+    <passport-personal-access-tokens></passport-personal-access-tokens>
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
