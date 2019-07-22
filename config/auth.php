51b07ff4b2d750e6868d772bad5b2e1d1bccd422
Sujan Prajapati <sujan@thewebsitemarketinggroup.com.au>
2019-07-22 15:50:59 +1000

    JWT setup

--- config/auth.php
+++ config/auth.php
@@ -42,7 +42,7 @@ return [
         ],
 
         'api' => [
-            'driver' => 'jwt.auth',
+            'driver' => 'jwt',
             'provider' => 'users',
             'hash' => false,
         ],
