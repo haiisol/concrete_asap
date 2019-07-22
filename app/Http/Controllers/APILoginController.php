4165f1d415ce68ea314f899939fae9e87602e7d6
Sujan Prajapati <sujan@thewebsitemarketinggroup.com.au>
2019-07-23 09:31:42 +1000

    User Details Model

--- app/Http/Controllers/APILoginController.php
+++ app/Http/Controllers/APILoginController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\Repositories\UserRepositoryInterface;
+use App\Repositories\Interfaces\UserRepositoryInterface;
 
 class APILoginController extends Controller
 {
