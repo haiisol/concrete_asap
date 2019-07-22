c071bba0499b16cba760a80a498abadd8cfb2624
Sujan Prajapati <sujan@thewebsitemarketinggroup.com.au>
2019-07-22 15:47:03 +1000

    JWT auth

--- config/auth.php
+++ config/auth.php
@@ -42,7 +42,7 @@ return [
         ],
 
         'api' => [
-            'driver' => 'passport',
+            'driver' => 'jwt.auth',
             'provider' => 'users',
             'hash' => false,
         ],
