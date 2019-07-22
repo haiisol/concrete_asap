99a405619f267f68554480a3a8c518b30de5e014
Sujan Prajapati <sujan@thewebsitemarketinggroup.com.au>
2019-07-22 12:27:57 +1000

    Passport changes

--- config/app.php
+++ config/app.php
@@ -176,7 +176,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         //Third party references
-        Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,        
+        // Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,        
 
     ],
 
