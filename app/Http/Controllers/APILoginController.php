287467387b426335816b49d15db9d1b98efe3744
Sujan Prajapati <sujan@thewebsitemarketinggroup.com.au>
2019-07-22 16:07:13 +1000

    API

--- app/Http/Controllers/APILoginController.php
+++ app/Http/Controllers/APILoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class APILoginController extends Controller
 {
@@ -37,7 +38,7 @@ class APILoginController extends Controller
     public function register(){
 
     }
-    
+
     /**
      * Get the guard to be used during authentication.
      *
