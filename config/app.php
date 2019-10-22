9fd5a02d1aacaf8b94cf8b5d3eb31f182291e27b
Sujan Prajapati <sujan@thewebsitemarketinggroup.com.au>
2019-10-22 12:14:56 +1100

    add custom provider in app

--- config/app.php
+++ config/app.php
@@ -174,9 +174,10 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        App\Providers\CustomPasswordResetServiceProvider::class,
         Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
         Barryvdh\Cors\ServiceProvider::class,
+
     ],
 
     /*
