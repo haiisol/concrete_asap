3eb75002de8d43c8337b08134821cc16183790ab
Sujan Prajapati <sujan@thewebsitemarketinggroup.com.au>
2019-10-23 09:44:41 +1100

    registration issue

--- app/Http/Controllers/APILoginController.php
+++ app/Http/Controllers/APILoginController.php
@@ -82,7 +82,7 @@ class APILoginController extends Controller
 
         if($validator->fails()){
             $errors = $validator->errors();
-            return response()->json(['message'=>$errors],401);
+            return response()->json(['message'=>"Please check the entered value.","errors"=>$errors],401);
         }
 
     	$user_details = $request->only('email', 'password','first_name','last_name','phone_number','abn','company','state','city','roles');
