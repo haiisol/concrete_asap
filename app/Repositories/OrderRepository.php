0f450a782c969c3292a35cd81588a6d8722fa08f
Sujan Prajapati <sujan@thewebsitemarketinggroup.com.au>
2019-10-25 11:51:21 +1100

    update bids order fix

--- app/Repositories/OrderRepository.php
+++ app/Repositories/OrderRepository.php
@@ -67,7 +67,7 @@ class OrderRepository implements Interfaces\OrderRepositoryInterface{
 
     public function getRepAllOrders($user_id){
         $orders=Order::with(["orderConcrete","bids"])->leftJoin('bids','bids.order_id','=','orders.id')
-            ->where("bids.user_id","!=",$user_id)->where("status","!=","trash")->get();
+            ->where("bids.user_id","!=",$user_id)->where("orders.status","!=","trash")->get();
 //        $orders=Order::with(["orderConcrete"])->get();
         return $orders;
     }
