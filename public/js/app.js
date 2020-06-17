3946cd36d0eb2ccaa2d74a2f1dd76f242c6d63be
Faisal <faisal.isol@gmail.com>
2020-06-17 11:37:58 +0700

    debug app.js

--- public/js/app.js
+++ public/js/app.js
@@ -37554,7 +37554,7 @@ var staticRenderFns = [
     var _h = _vm.$createElement
     var _c = _vm._self._c || _h
     return _c("div", { staticClass: "col-md-12" }, [
-      _c("h4", { staticClass: "font-bold" }, [_vm._v("Order Table tst")])
+      _c("h4", { staticClass: "font-bold" }, [_vm._v("Order Table")])
     ])
   }
 ]
