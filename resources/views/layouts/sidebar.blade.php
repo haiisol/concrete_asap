dc975f173a673f211c4703b047e424c1f71a1db6
Hendra Wijaya <hendra@twmg.com.au>
2020-06-18 14:16:03 +0700

    add data table

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -31,7 +31,7 @@
             </a>
          </li>
          <li>
-            <a href="/">
+            <a href="/contractor">
                <i class="nc-icon nc-settings"></i>
                <p>Contractor</p>
             </a>
