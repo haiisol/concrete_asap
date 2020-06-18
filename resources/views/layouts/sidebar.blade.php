7083ca72f30c5ff6de7b292268e05b877439c170
Faisal <faisal.isol@gmail.com>
2020-06-18 09:49:36 +0700

    add concrete and rep menu

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -3,13 +3,13 @@
       Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red | yellow"
       -->
    <div class="logo">
-      <a href="http://www.creative-tim.com" class="simple-text logo-mini">
+      <a href="<?php echo url('/'); ?>" class="simple-text logo-mini">
          <div class="logo-image-small">
             <i class="fa fa-user fa-2x"></i>
          </div>
       </a>
-      <a href="http://www.creative-tim.com" class="simple-text logo-normal">
-         Concrete ASAP
+      <a href="<?php echo url('/'); ?>" class="simple-text logo-normal">
+         Administrator
          <!-- <div class="logo-image-big">
             <img src="../assets/img/logo-big.png">
             </div> -->
@@ -19,25 +19,25 @@
       <ul class="nav">
          <li class="active ">
             <a href="/home">
-               <i class="nc-icon nc-bank"></i>
+               <i class="nc-icon nc-shop"></i>
                <p>Dashboard</p>
             </a>
          </li>
          <li>
             <a href="/order">
-               <i class="nc-icon nc-diamond"></i>
+               <i class="nc-icon nc-cart-simple"></i>
                <p>Order</p>
             </a>
          </li>
          <li>
             <a href="/">
-               <i class="nc-icon nc-diamond"></i>
+               <i class="nc-icon nc-settings"></i>
                <p>Contractor</p>
             </a>
          </li>
          <li>
             <a href="/">
-               <i class="nc-icon nc-diamond"></i>
+               <i class="nc-icon nc-single-02"></i>
                <p>REP</p>
             </a>
          </li>
