1d409752f38fca93fc5ebf2c8d709c8f48219248
Hendra Wijaya <hendra@twmg.com.au>
2020-06-18 16:32:58 +0700

    add contractor controller

--- resources/js/components/table/ContractorTableComponent.vue
+++ resources/js/components/table/ContractorTableComponent.vue
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="row justify-content-center">
             <div class="col-md-12">
-                <h4 class="font-bold">Order Table</h4>
+                <h4 class="font-bold">Contractor Table</h4>
             </div>
             <div class="col-md-12">
                 <table class="table table-striped table-bordered" id="dataTableDisplayVue" style="width:100%">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,20 +35,20 @@
                       <nav class="footer-nav">
                          <ul>
                             <li>
-                               <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a>
+                               <a href="/" target="_blank">TWMG</a>
                             </li>
                             <li>
-                               <a href="http://blog.creative-tim.com/" target="_blank">Blog</a>
+                               <a href="/" target="_blank">Blog</a>
                             </li>
                             <li>
-                               <a href="https://www.creative-tim.com/license" target="_blank">Licenses</a>
+                               <a href="/" target="_blank">Licenses</a>
                             </li>
                          </ul>
                       </nav>
                       <div class="credits ml-auto">
                          <span class="copyright">
                             ©
-                           2019, made with <i class="fa fa-heart heart"></i> by Creative Tim
+                           2019, made with <i class="fa fa-heart heart"></i> by TWMG
                          </span>
                       </div>
                    </div>
