3c7c617d0cc55afbc5b8aca0e2fb7f5464d7e104
Hendra Wijaya <hendra@twmg.com.au>
2020-06-19 11:32:36 +0700

    add logo on admin login and change btn color

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -16,6 +16,7 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/paper-dashboard.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/admin-login.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
