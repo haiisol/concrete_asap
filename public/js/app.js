49e915d87456529e08244875c5307814b0d4b9d1
Hendra Wijaya <hendra@twmg.com.au>
2020-06-22 15:33:23 +0700

    add bids information on rep details

--- public/js/app.js
+++ public/js/app.js
@@ -38319,7 +38319,7 @@ var staticRenderFns = [
     var _h = _vm.$createElement
     var _c = _vm._self._c || _h
     return _c("div", { staticClass: "col-md-12" }, [
-      _c("h4", { staticClass: "font-bold" }, [_vm._v("Posted Jobs")])
+      _c("h4", { staticClass: "font-bold" }, [_vm._v("Bids")])
     ])
   }
 ]
