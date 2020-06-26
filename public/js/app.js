22398e12b04ebaa0305af7737cdfe55f477eb689
Faisal <faisal@thewebsitemarketinggroup.com.au>
2020-06-26 15:42:02 +0700

    update datatable css and js

--- public/js/app.js
+++ public/js/app.js
@@ -51824,8 +51824,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\concrete_asap\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\concrete_asap\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\concrete\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\concrete\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
