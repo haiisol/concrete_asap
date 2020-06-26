b136776ebef80a755c038df2d61a678f62fadd15
Hendra Wijaya <hendra@twmg.com.au>
2020-06-26 14:34:10 +0700

    remove admin footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,31 +31,6 @@
              <div class="content">
                 @yield('content')
              </div>
-             <footer class="footer footer-black  footer-white ">
-                <div class="container-fluid">
-                   <div class="row">
-                      <nav class="footer-nav">
-                         <ul>
-                            <li>
-                               <a href="/" target="_blank">TWMG</a>
-                            </li>
-                            <li>
-                               <a href="/" target="_blank">Blog</a>
-                            </li>
-                            <li>
-                               <a href="/" target="_blank">Licenses</a>
-                            </li>
-                         </ul>
-                      </nav>
-                      <div class="credits ml-auto">
-                         <span class="copyright">
-                            ©
-                           2019, made with <i class="fa fa-heart heart"></i> by TWMG
-                         </span>
-                      </div>
-                   </div>
-                </div>
-             </footer>
           </div>
        </div>
     </div>
